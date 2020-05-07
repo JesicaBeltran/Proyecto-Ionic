ed98cc6f0edb69d32d82cd534a0b1ad024f6ea8c
Jesica <jessbc@gmail.com>
2020-05-07 12:45:24 +0200

    Realizada peticion Post a Api

---
+++ src/app/Paginas/crearAviso/crearAviso.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../services/rest.service';
+
+@Component({
+  selector: 'app-crearAviso',
+  templateUrl: './crearAviso.page.html',
+  styleUrls: ['./crearAviso.page.scss'],
+})
+export class ExistenciasPage implements OnInit {
+
+arrayNewAviso:any;
+  constructor(public servicioPost:RestService) { 
+    //this.supermercado = this.supermercado;
+
+  }
+
+  ngOnInit() {
+  }
+
+  crearAviso(supermercado: string, producto: string, comentario: string){
+    this.arrayNewAviso={"supermercado":supermercado,"producto":producto, "comentario":comentario};
+    console.log(this.arrayNewAviso);
+    this.getAvisoCreado();
+    
+
+  }
+
+  getAvisoCreado(){
+
+    this.servicioPost.postAviso(this.arrayNewAviso).then(data => {
+      this.arrayNewAviso=data;
+      console.log(this.arrayNewAviso);
+     
+    }).catch(data => {
+      console.log(data);
+    })
+  
+  }
+
+  
+}
--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -6,17 +6,40 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RestService {
   constructor(public Http: HttpClient) { }
-//PETICION GET A API REST
+
   ngOnInit(){
-    var api_url="https://localhost:44394/api/GetAvisoItems";
-    return new Promise (resolve =>
-    this.Http.get(api_url).subscribe(data => {
-    resolve(data);
     
-    }, err => {
-      console.log(err);
-    })
-      )
 }
+//PETICION GET A API REST
+getAviso(){
+  var api_url="https://localhost:44394/api/GetAvisoItems";
+  return new Promise (resolve =>
+  this.Http.get(api_url).subscribe(data => {
+  resolve(data);
+  
+  }, err => {
+    console.log(err);
+  })
+    )
+}
+postAviso(arrayNewAviso: any){
+
+  let datos = arrayNewAviso;
+  
+  let options = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+ var url = 'https://localhost:44394/api/AddAvisoItems';
+ return new Promise(resolve => {
+  this.Http.post(url,JSON.stringify(datos),options)
+     .subscribe(data => {
+       resolve(data);
+      });
+ });
+  
+}
+
 
 }
